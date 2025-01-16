036c5f15b179fd38ed9e361112c1e8df0c36458e
dreamCat <61037865+yimeng261@users.noreply.github.com>
2025-01-16 08:15:42 +0800

    Delete 梦云喵拓展包_v0.1 directory

--- 梦云喵拓展包_v0.1/kubejs/client_scripts/dlc/dlc_jei.js
+++
@@ -1,3 +0,0 @@
-JEIEvents.addItems(event=>{
-    event.addItem('kubejs:pigment_bubble', Text.black('颜料泡泡由扭曲泡泡在器官容器中进化获得'))
-});
--- 梦云喵拓展包_v0.1/kubejs/server_scripts/dlc/dlc_loot.js
+++
@@ -1,9 +0,0 @@
-const dlcLoot={
-    'kubejs:recycle_system':function(event,organ){
-        let player = event.killerEntity;
-        let count = player.persistentData.getInt(resourceCount);
-        player.persistentData.putInt(resourceCount,count+5);
-    }
-}
-
-Object.assign(entityLootStrategies,dlcLoot);
