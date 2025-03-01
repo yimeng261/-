6bcc3d5b8de229307ddd6845265ad24502e5426a
dreamCat <61037865+yimeng261@users.noreply.github.com>
2025-03-02 07:52:12 +0800

    蜘蛛丝腺提示

--- 梦云喵拓展包_v0.20/kubejs/client_scripts/dlc/dlc_jei.js
+++ 梦云喵拓展包_v0.20/kubejs/client_scripts/dlc/dlc_jei.js
@@ -2,4 +2,5 @@ JEIEvents.information(event => {
     event.addItem('kubejs:tree_heart', Text.black('开始种树吧~'))
     event.addItem('kubejs:sarcoma_core', Text.black('在器官容器中用肿瘤之源进化获得'))
     event.addItem('kubejs:flower_of_heart',Text.black('在器官容器中用花之心进化获得'))
+    event.addItem('kubejs:spider_gland',Text.black('在洞穴蜘蛛胸腔中获得或者击杀蜘蛛有概率掉落'))
 })
