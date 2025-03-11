c6ce20bcb4c8483eba69bb4904adb33c290875c4
dreamCat <61037865+yimeng261@users.noreply.github.com>
2025-03-11 17:30:05 +0800

    Delete 梦云喵拓展包_v0.20 directory

--- 梦云喵拓展包_v0.20/kubejs/startup_scripts/dlc/dlc_custom_events.js
+++
@@ -1,7 +0,0 @@
-
-ForgeEvents.onEvent('net.minecraftforge.event.entity.living.MobEffectEvent$Added',event=>{
-    if(!event.entity.isPlayer()) return;
-    global.dlc_effect(event);
-    event.effectInstance.effect.isBeneficial();
-    
-})
--- 梦云喵拓展包_v0.20/kubejs/startup_scripts/dlc/dlc_key_bind.js
+++
@@ -1,4 +0,0 @@
-global.ScanType = new $KeyMapping('记录矿物类型', $GLFWKey.GLFW_KEY_Z, 'key.categories.kubejs')
-StartupEvents.init(event => {
-    $KeyMappingRegistry.register(global.ScanType)
-})
