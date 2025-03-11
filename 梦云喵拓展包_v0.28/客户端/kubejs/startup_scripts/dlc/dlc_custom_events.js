bfdb6a819b0e34da616fafffa1c04087b1ce0361
yimeng261 <zhuhengyi2@163.com>
2025-03-11 17:21:48 +0800

    修复和创哥dlc冲突的问题

--- 梦云喵拓展包_v0.28/客户端/kubejs/startup_scripts/dlc/dlc_custom_events.js
+++ 梦云喵拓展包_v0.28/客户端/kubejs/startup_scripts/dlc/dlc_custom_events.js
@@ -1,7 +1,6 @@
 
 ForgeEvents.onEvent('net.minecraftforge.event.entity.living.MobEffectEvent$Added',event=>{
     if(!event.entity.isPlayer()) return;
+    if(event.entity.level.isClientSide()) return;
     global.dlc_effect(event);
-    event.effectInstance.effect.isBeneficial();
-    
 })
